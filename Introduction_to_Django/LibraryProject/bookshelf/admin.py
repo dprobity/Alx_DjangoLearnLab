d51e85b65edecaec2217a7eac792d502bf528cca
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-18 20:34:21 -0600

    Pushed some codes here

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,5 @@ class Book_admin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
     list_filter = ('author', 'publication_year')  # Filters for easy search
     search_fields = ('title', 'author')  # Search bar for title and author
+
+admin.site.register(Book,Book_admin)  # This line makes sure the model is register and can be visualised in the admin interface
