30c2eaa2414a4290489a6224ee390c96b6a250e4
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-23 06:53:50 -0600

    Pushed codes here

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Book, Library
 # Function-based view to list all books with their authors
 def list_books(request):
     books = Book.objects.all()  # Query all books
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 # Class-based view to display details for a specific library,
 # including a list of all books available in that library.
