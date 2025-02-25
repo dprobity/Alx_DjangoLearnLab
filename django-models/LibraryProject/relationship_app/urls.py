b14c4ed790c9e634175ec37626896840f8ef8e97
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-25 14:46:16 -0600

    pushed

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
      # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/relationship_app/login/'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
