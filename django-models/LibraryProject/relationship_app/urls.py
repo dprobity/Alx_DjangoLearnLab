e9c153e073dba8482aea18f39fe6100d00509431
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-25 14:44:11 -0600

    pushed

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,11 +11,15 @@ urlpatterns = [
     # Here, <int:pk> is used to pass the primary key of the Library.
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
      # Authentication URLs using Django's built-in views
-    path('login/', auth_views.LoginView.as_view(
-        template_name='registration/login.html',
-        redirect_authenticated_user=True,
-        next_page='/relationship_app/books/'  # Explicit redirect
-    ), name='login'),    
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(
+    #     template_name='registration/login.html',
+    #     redirect_authenticated_user=True,
+    #     next_page='/relationship_app/books/'  # Explicit redirect
+    # ), name='login'),    
+    # path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+
+     # Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/relationship_app/login/'), name='logout'),
     path('register/', register, name='register'),
 ]
