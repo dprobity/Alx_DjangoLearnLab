ccdb56522693593160691a8ce0493d0868dc5751
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-02-25 21:10:24 -0600

    PPPP

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,18 +6,33 @@ from django.dispatch import receiver
 
 # Define User roles
 
-ROLE_CHOICES = [
-    ('Admin', 'Admin'),
-    ('Librarin', 'Librarian'),
-    ('Member', 'Member'),
-]
+# ROLE_CHOICES = [
+#     ('Admin', 'Admin'),
+#     ('Librarin', 'Librarian'),
+#     ('Member', 'Member'),
+
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+#     def __str__(self):
+#         return f"{self.user.username} - {self.role}"
+
+from django.db import models
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+    role = models.CharField(max_length=20, choices=[
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ], default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
