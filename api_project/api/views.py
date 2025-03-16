eb73c3c921dfdd5b46bb0a035cc7dfdbdda0af61
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-03-16 02:49:02 -0500

    first commit for task1

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
 
 # Create your views here.
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()              #  Retrieve all the books in the db
+    serializer_class = BookSerializer           # Serialise or convert retrieved books to a JSON for the frontend to consume 
+
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),       # Just to note this is the default admin url that can be accessed when u create a superuser
+    path('api/', include('api.urls')),   # This line includes all api-app related urls 
+
 ]
