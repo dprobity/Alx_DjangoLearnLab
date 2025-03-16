96ca18fcf6347468d3caa088a4b133e7df503803
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-03-16 05:24:44 -0500

    1st  commit for task2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,13 +4,17 @@ from rest_framework import generics, viewsets
 from .models import Books
 from .serializers import BookSerializer
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    """A viewset for viewing an deditiing book instances."""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 class BookList(generics.ListAPIView):
     queryset = Books.objects.all()  # Retrieve all books from the database
     serializer_class = BookSerializer  # Convert books to JSON format
 
 
-class BookViewSet(viewsets.ModelViewSet):
-    """A viewset for viewing an deditiing book instances."""
 
-    queryset = Books.objects.all()
-    serializer_class = BookSerializer
