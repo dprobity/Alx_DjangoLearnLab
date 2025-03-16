01ac1f2c471076e6d8b757194791a32b42ec55ce
Emehelu Amobi David <dprobityconcept@gmail.com>
2025-03-16 03:02:19 -0500

    3rd  commit for task1

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
+#from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Retrieve all books from the database
     serializer_class = BookSerializer  # Convert books to JSON format
